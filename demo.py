726d3cacbf61507f8aaa45ecf5ad2279a5ed0cad
Demkov Boris <demkov.boris@gmail.com>
2025-03-19 00:22:38 +0300

    add options

---
+++ demo.py
@@ -0,0 +1,23 @@
+def example_function(param1, param2, param3):
+    # This is a poorly formatted function
+    result = param1 + param2 * param3
+    if result > 100:
+        print("Result is too large!")
+    else:
+        print(f"Result is {result}")
+    return {
+        "param1": param1,
+        "param2": param2,
+        "param3": param3,
+        "param3": param3,
+        "param3": param3,
+        "param3": param3,
+    }
+
+
+class ExampleClass:
+    def __init__(self, name):
+        self.name = name
+
+    def greet(self):
+        print(f"Hello, {self.name}!")
